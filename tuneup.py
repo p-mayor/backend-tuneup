ca28be538dc751363109ee5093ef54641dc6a321
Peter <peter@Peters-MBP-2.localdomain>
2019-06-17 11:11:05 -0400

    refactored to 0.003 sec per call

--- tuneup.py
+++ tuneup.py
@@ -9,20 +9,18 @@ import pstats
 import timeit
 
 
-def profiler():
-    def inner(func):
-        pr = cProfile.Profile()
-        pr.enable()
-        result = func()
-        pr.disable()
-        sortby = 'cumulative'
-        return result
-    return inner
-
-
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
-    raise NotImplementedError("Complete this decorator function")
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        result = func(*args, **kwargs)
+        pr.disable()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr).sort_stats(sortby)
+        ps.print_stats()
+        return result
+    return inner
 
 
 def read_movies(src):
@@ -43,14 +41,28 @@ def is_duplicate(title, movies):
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
+    formatted_movies = []
+    for item in movies:
+        formatted_movies.append(item.split('\t')[1])
+    seen = {}
     duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
+    # original solution
+    # while movies:
+    #     movie = movies.pop()
+    #     if is_duplicate(movie, movies):
+    #         duplicates.append(movie)
+
+    for movie in formatted_movies:
+        if movie in seen:
+            seen[movie] += 1
             duplicates.append(movie)
-    return duplicates
+        else:
+            seen[movie] = 1
+
+    print (duplicates)
 
 
+@profile
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
     # YOUR CODE GOES HERE
